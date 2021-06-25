743bfef8efb2b3b7a6a4461fd70893525741a0f5
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-06-25 01:42:57 -0500

    Generado

---
+++ Inventory/ui/WindowSearchProduct.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Inventory.ui
+{
+	public partial class WindowSearchProduct : Window
+	{
+		public WindowSearchProduct()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
