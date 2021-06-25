df5be56d7b9b338459a2049321bf008582a906d5
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-06-25 01:24:42 -0500

    Movido MainWindow a \ui

---
+++ Inventory/ui/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Inventory.ui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
