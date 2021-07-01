0dfeb864933cf363878c8224d40283b9eb348972
Etiensro <hernandezordaze@gmail.com>
2021-07-01 11:25:11 -0500

    Finalizado diseño sencillo

--- Inventory/ui/LoginWindow.xaml.cs
+++ Inventory/ui/LoginWindow.xaml.cs
@@ -2,9 +2,6 @@
 
 namespace Inventory.ui
 {
-	/// <summary>
-	/// Lógica de interacción para LoginWindow.xaml
-	/// </summary>
 	public partial class LoginWindow : Window
 	{
 		public LoginWindow()
