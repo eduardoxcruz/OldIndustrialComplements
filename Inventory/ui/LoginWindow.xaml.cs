e36dfd0f35234b2fa3bd223533aa2f22bc34ed0a
Etiensro <hernandezordaze@gmail.com>
2021-07-01 10:14:39 -0500

    Generado

---
+++ Inventory/ui/LoginWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Inventory.ui
+{
+	/// <summary>
+	/// Lógica de interacción para LoginWindow.xaml
+	/// </summary>
+	public partial class LoginWindow : Window
+	{
+		public LoginWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
