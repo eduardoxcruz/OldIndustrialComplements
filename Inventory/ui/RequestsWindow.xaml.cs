99d61ab1ac281d4a4fd5af3304f1ab408db23c43
Etiensro <hernandezordaze@gmail.com>
2021-07-01 16:52:27 -0500

    Finalizado diseño sencillo

--- Inventory/ui/RequestsWindow.xaml.cs
+++ Inventory/ui/RequestsWindow.xaml.cs
@@ -2,9 +2,6 @@
 
 namespace Inventory.ui
 {
-	/// <summary>
-	/// Lógica de interacción para RequestsWindow.xaml
-	/// </summary>
 	public partial class RequestsWindow : Window
 	{
 		public RequestsWindow()
