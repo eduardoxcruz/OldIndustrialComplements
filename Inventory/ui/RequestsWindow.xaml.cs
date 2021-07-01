a0bc48613d174ca899d9d392ed0831a00ddd7d71
Etiensro <hernandezordaze@gmail.com>
2021-07-01 16:22:32 -0500

    Generado

---
+++ Inventory/ui/RequestsWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Inventory.ui
+{
+	/// <summary>
+	/// Lógica de interacción para RequestsWindow.xaml
+	/// </summary>
+	public partial class RequestsWindow : Window
+	{
+		public RequestsWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
