ce7daf3eafa10a731fa605d4b7764c0adc60cf1c
Etiensro <hernandezordaze@gmail.com>
2021-07-01 11:31:03 -0500

    Generado

---
+++ Inventory/ui/DetailsWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Inventory.ui
+{
+	/// <summary>
+	/// Lógica de interacción para DetailsWindow.xaml
+	/// </summary>
+	public partial class DetailsWindow : Window
+	{
+		public DetailsWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
