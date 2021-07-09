a47ec18fde987e602e2d9aa425bf7fa3a00e97b9
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-07-09 13:20:24 -0500

    Test de conexion simple a la BD mediante la clase SqlConnection

--- Inventory/ui/ConnectionsWindow.xaml.cs
+++ Inventory/ui/ConnectionsWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows;
 
 namespace Inventory.ui
 {
@@ -7,6 +9,22 @@ namespace Inventory.ui
 		public ConnectionsWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void BtnTestConnection_OnClick(object sender, RoutedEventArgs routedEventArgs)
+		{
+			string connectionString = "Server=192.168.0.254,1433;Database=Pruebas;Integrated Security=False;User Id=eduardo;Password=Cruz0320;MultipleActiveResultSets=True";
+			SqlConnection sqlDatabaseConnection = new SqlConnection(connectionString);
+			try
+			{
+				sqlDatabaseConnection.Open();
+				TxtBlockConnectionResult.Text = "Conectado";
+			}
+			catch (SqlException exception)
+			{
+				TxtBlockConnectionResult.Text = "Hubo un error: " + exception;
+				throw;
+			}
 		}
 	}
 }
