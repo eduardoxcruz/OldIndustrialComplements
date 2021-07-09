372c0e0bffd489b05a69f7a11f9de28f7621c095
Etiensro <hernandezordaze@gmail.com>
2021-07-09 15:13:20 -0500

    Extraidas las propiedades de Connections a la clase SqlDatabase

--- Inventory/database/SqlDatabase.cs
+++ Inventory/database/SqlDatabase.cs
@@ -2,6 +2,14 @@
 {
 	public class SqlDatabase
 	{
+		static string serverIp = "192.168.0.254";
+		static string serverPort = "1433";
+		static string databaseName = "Pruebas";
+		static string integratedSecurity = "False";
+		static string userId = "eduardo";
+		static string password = "Cruz0320";
+		static string multipleActiveResultSets = "True";
+		private static string databaseConnectionString = "Server=" + serverIp + "," + serverPort + "," + databaseName + "," + integratedSecurity + "," + userId + "," + password + "," + multipleActiveResultSets;
 		
 	}
 }
