188a3fc53b7d554bbdcf744c2aae60c0d687c4dd
Etiensro <hernandezordaze@gmail.com>
2021-07-09 17:11:17 -0500

    Generado metodo para iniciar el cliente sql

--- Inventory/database/SqlDatabase.cs
+++ Inventory/database/SqlDatabase.cs
@@ -1,4 +1,6 @@
-﻿namespace Inventory.database
+﻿using System.Data.SqlClient;
+
+namespace Inventory.database
 {
 	public class SqlDatabase
 	{
@@ -9,7 +11,12 @@
 		static string userId = "eduardo";
 		static string password = "Cruz0320";
 		static string multipleActiveResultSets = "True";
-		private static string databaseConnectionString = "Server=" + serverIp + "," + serverPort + "," + databaseName + "," + integratedSecurity + "," + userId + "," + password + "," + multipleActiveResultSets;
-		
+
+		static string databaseConnectionString = "Server=" + serverIp + "," + serverPort + ";Database=" + databaseName + ";Integrated Security=" + integratedSecurity + ";User Id=" + userId + ";Password=" + password + ";MultipleActiveResultSets=" + multipleActiveResultSets;
+		public void StartSqlClient()
+		{
+			SqlConnection sqlDatabaseConnection = new SqlConnection(databaseConnectionString);
+			sqlDatabaseConnection.Open();
+		}
 	}
-}
+}	
