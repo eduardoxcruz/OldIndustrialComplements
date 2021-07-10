c4518ff4c14202757287ba06e0650fe56375ee2b
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-07-09 21:21:56 -0500

    Reformateadas propiedades a constantes y con formate CamelCase segun la nomenclatura de C#

--- Inventory/database/SqlDatabase.cs
+++ Inventory/database/SqlDatabase.cs
@@ -4,15 +4,15 @@ namespace Inventory.database
 {
 	public class SqlDatabase
 	{
-		static string serverIp = "192.168.0.254";
-		static string serverPort = "1433";
-		static string databaseName = "Pruebas";
-		static string integratedSecurity = "False";
-		static string userId = "eduardo";
-		static string password = "Cruz0320";
-		static string multipleActiveResultSets = "True";
+		const string ServerIp = "192.168.0.254";
+		const string ServerPort = "1433";
+		const string DatabaseName = "Pruebas";
+		const string IntegratedSecurity = "False";
+		const string UserId = "eduardo";
+		const string Password = "Cruz0320";
+		const string MultipleActiveResultSets = "True";
 
-		static string databaseConnectionString = "Server=" + serverIp + "," + serverPort + ";Database=" + databaseName + ";Integrated Security=" + integratedSecurity + ";User Id=" + userId + ";Password=" + password + ";MultipleActiveResultSets=" + multipleActiveResultSets;
+		static string databaseConnectionString = "Server=" + ServerIp + "," + ServerPort + ";Database=" + DatabaseName + ";Integrated Security=" + IntegratedSecurity + ";User Id=" + UserId + ";Password=" + Password + ";MultipleActiveResultSets=" + MultipleActiveResultSets;
 		public void StartSqlClient()
 		{
 			SqlConnection sqlDatabaseConnection = new SqlConnection(databaseConnectionString);
