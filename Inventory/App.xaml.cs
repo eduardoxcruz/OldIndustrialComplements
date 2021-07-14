c119a252feca5dbf07d9301124187cb83dc27800
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-07-14 17:45:09 -0500

    Agregados metodos para detectar si una ventana esta abierta a partir del nombre de su tipo

--- Inventory/App.xaml.cs
+++ Inventory/App.xaml.cs
@@ -1,17 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 
 namespace Inventory
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+	/// <summary>
+	///     Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		public static bool IsWindowOpen<T>(string windowName = "") where T : Window
+		{
+			return string.IsNullOrEmpty(windowName)
+				? Current.Windows.OfType<T>().Any()
+				: Current.Windows.OfType<T>().Any(w => w.GetType().Name.Equals(windowName));
+		}
+
+		public static bool IsWindowOpen(string windowName = "")
+		{
+			return string.IsNullOrEmpty(windowName)
+				? Current.Windows.OfType<Window>().Any()
+				: Current.Windows.OfType<Window>().Any(w => w.GetType().Name.Equals(windowName));
+		}
+	}
 }
