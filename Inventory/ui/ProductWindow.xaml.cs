102a85ea3285f84ef9f7396bbca0204576b2d594
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-07-14 15:14:19 -0500

    Agregada propiedad Task con Get y Set

--- Inventory/ui/ProductWindow.xaml.cs
+++ Inventory/ui/ProductWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Inventory.ui
 {
@@ -23,7 +11,24 @@ namespace Inventory.ui
 
 	public partial class ProductWindow : Window
 	{
-		public ProductWindow()
+		private int _task = 0;
+
+		private int Task
+		{
+			get
+			{
+				return _task;
+			}
+			set
+			{
+				if (value < 3)
+				{
+					_task = value;
+				}
+			}
+		}
+
+		public ProductWindow(int task)
 		{
 			InitializeComponent();
 		}
