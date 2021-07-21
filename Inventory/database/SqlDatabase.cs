c36e0f6423da0478ab3cac9cc547290595f5c2b0
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-07-21 10:26:31 -0500

    Propiedades de la base de datos asignados explicitamente como privados. Encapsulado la DatabaseConnectionString

--- Inventory/database/SqlDatabase.cs
+++ Inventory/database/SqlDatabase.cs
@@ -4,15 +4,30 @@ namespace Inventory.database
 {
 	public class SqlDatabase
 	{
-		const string ServerIp = "192.168.0.254";
-		const string ServerPort = "1433";
-		const string DatabaseName = "Pruebas";
-		const string IntegratedSecurity = "False";
-		const string UserId = "eduardo";
-		const string Password = "Cruz0320";
-		const string MultipleActiveResultSets = "True";
+		private const string ServerIp = "192.168.0.254";
+		private const string ServerPort = "1433";
+		private const string DatabaseName = "Pruebas";
+		private const string IntegratedSecurity = "False";
+		private const string UserId = "eduardo";
+		private const string Password = "Cruz0320";
+		private const string MultipleActiveResultSets = "True";
+		private string _databaseConnectionString;
 
-		public const string DatabaseConnectionString = "Server=" + ServerIp + "," + ServerPort + ";Database=" + DatabaseName + ";Integrated Security=" + IntegratedSecurity + ";User Id=" + UserId + ";Password=" + Password + ";MultipleActiveResultSets=" + MultipleActiveResultSets;
+		
+		public string DatabaseConnectionString
+		{
+			get
+			{
+				return _databaseConnectionString;
+			}
+			set
+			{
+				if (!string.IsNullOrEmpty(value))
+				{
+					_databaseConnectionString = value;
+				}
+			}
+		}
 		public SqlConnection StartSqlClient()
 		{
 			SqlConnection sqlDatabaseConnection = new SqlConnection(DatabaseConnectionString);
