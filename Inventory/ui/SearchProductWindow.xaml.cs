d3a33030c78649afa5e3ac7303c9a0dbe4ce49b8
Etiensro <hernandezordaze@gmail.com>
2021-07-29 13:11:30 -0500

    Limitada la cantidad de busquedas en el datagrid a 25 resultados

--- Inventory/ui/SearchProductWindow.xaml.cs
+++ Inventory/ui/SearchProductWindow.xaml.cs
@@ -32,7 +32,7 @@ namespace Inventory.ui
 
 		private void SearchProductWithQuickSearch(string text)
 		{
-			string query = "SELECT * FROM dbo.productos2 WHERE id like('%" + text + "%') OR matricula like ('%" + text + "%') OR descripcion like ('%" + text + "%') OR contenedor like ('%" + text + "%') OR ubicacion like ('%" + text + "%') OR estado like ('%"+ text + "%') OR tecmon like ('%" + text + "%') OR encapsulado like ('%" + text + "%')";
+			string query = "SELECT TOP 25 * FROM dbo.productos2 WHERE id like('%" + text + "%') OR matricula like ('%" + text + "%') OR descripcion like ('%" + text + "%') OR contenedor like ('%" + text + "%') OR ubicacion like ('%" + text + "%') OR estado like ('%"+ text + "%') OR tecmon like ('%" + text + "%') OR encapsulado like ('%" + text + "%')";
 			sqlDatabase.FillDataGridWithQuery(query, DataGridProducts);
 		}
 
@@ -44,7 +44,7 @@ namespace Inventory.ui
 				return;
 			}
 
-			string query = "SELECT * FROM dbo.productos2 WHERE id like('%" + TxtBoxId.Text + "%') AND matricula like ('%" + TxtBoxEnrollment.Text + "%') AND descripcion like ('%" + TxtBoxDescription.Text + "%') AND contenedor like ('%" + TxtBoxContainer.Text + "%') AND ubicacion like ('%" + TxtBoxLocation.Text + "%') AND estado like ('%" + TxtBoxStatus.Text + "%') AND tecmon like ('%" + TxtBoxMountingTechnology.Text + "%')AND encapsulado like ('%" + TxtBoxEncapsulation.Text + "%')AND codigo like ('%" + TxtBoxDebugCode.Text + "%')";
+			string query = "SELECT TOP 25 * FROM dbo.productos2 WHERE id like('%" + TxtBoxId.Text + "%') AND matricula like ('%" + TxtBoxEnrollment.Text + "%') AND descripcion like ('%" + TxtBoxDescription.Text + "%') AND contenedor like ('%" + TxtBoxContainer.Text + "%') AND ubicacion like ('%" + TxtBoxLocation.Text + "%') AND estado like ('%" + TxtBoxStatus.Text + "%') AND tecmon like ('%" + TxtBoxMountingTechnology.Text + "%')AND encapsulado like ('%" + TxtBoxEncapsulation.Text + "%')AND codigo like ('%" + TxtBoxDebugCode.Text + "%')";
 			sqlDatabase.FillDataGridWithQuery(query, DataGridProducts);
 		}
 
