f00b660cbb802de7e38b4d5d01c98ffa2e06581a
Etiensro <hernandezordaze@gmail.com>
2021-08-03 17:18:20 -0500

    Agregados eventos click y loaded para guardar la informacion de la ip del servidor y el path de las fotos

--- Inventory/ui/SettingsWindow.xaml.cs
+++ Inventory/ui/SettingsWindow.xaml.cs
@@ -8,5 +8,24 @@ namespace Inventory.ui
 		{
 			InitializeComponent();
 		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			if (string.IsNullOrEmpty(TxtBoxIpAdress.Text) || string.IsNullOrEmpty(TxtxBoxSystemAdress.Text))
+			{
+				MessageBox.Show("Ningun campo puede quedar vacio");
+				return;
+			}
+
+			Properties.Settings.Default.DatabaseIp = TxtBoxIpAdress.Text;
+			Properties.Settings.Default.PhotosPath = TxtxBoxSystemAdress.Text;
+			Properties.Settings.Default.Save();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			TxtBoxIpAdress.Text = Properties.Settings.Default.DatabaseIp;
+			TxtxBoxSystemAdress.Text = Properties.Settings.Default.PhotosPath;
+		}
 	}
 }
