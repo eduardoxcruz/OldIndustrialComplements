b7d738e1a38a5d77fcd3ce254cabe6290825d13e
Etiensro <hernandezordaze@gmail.com>
2021-08-03 17:14:12 -0500

    Creado evento Loaded para LoginWindow para cargar los datos guardados, evento checked para el chkBoxRememberData para comprobar que los campos no esten vacios y evento click para realizar el guardado de datos

--- Inventory/ui/LoginWindow.xaml.cs
+++ Inventory/ui/LoginWindow.xaml.cs
@@ -8,5 +8,47 @@ namespace Inventory.ui
 		{
 			InitializeComponent();
 		}
+
+		private void ChkBoxRememberData_Checked(object sender, RoutedEventArgs e)
+		{
+			if (string.IsNullOrEmpty(TxtBoxPassword.Password) && string.IsNullOrEmpty(TxtxBoxUser.Text))
+			{
+				ChkBoxRememberData.IsChecked = false;
+				MessageBox.Show("Introduce datos para guardar la información");
+			}
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			TxtxBoxUser.Text = Properties.Settings.Default.User;
+			TxtBoxPassword.Password = Properties.Settings.Default.Pass;
+			ChkBoxRememberData.IsChecked = Properties.Settings.Default.SaveSession;
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			if (string.IsNullOrEmpty(TxtBoxPassword.Password) && string.IsNullOrEmpty(TxtxBoxUser.Text))
+			{
+				ChkBoxRememberData.IsChecked = false;
+				MessageBox.Show("Introduce datos para guardar la información");
+			}
+			else
+			{
+				if (ChkBoxRememberData.IsChecked == true)
+				{
+					Properties.Settings.Default.User = TxtxBoxUser.Text;
+					Properties.Settings.Default.Pass = TxtBoxPassword.Password;
+					Properties.Settings.Default.SaveSession = true;
+					Properties.Settings.Default.Save();
+				}
+				else if (ChkBoxRememberData.IsChecked == false)
+				{
+					Properties.Settings.Default.User = "";
+					Properties.Settings.Default.Pass = "";
+					Properties.Settings.Default.SaveSession = false;
+					Properties.Settings.Default.Save();
+				}
+			}
+		}
 	}
 }
