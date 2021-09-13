6cd5adbdf9ecaddaa95916534835ddb1e98ed896
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-09-13 12:14:13 -0500

    Agregada propiedad SqlDataTable

--- Inventory/data/SqlDataGrid.cs
+++ Inventory/data/SqlDataGrid.cs
@@ -6,15 +6,22 @@ namespace Inventory.data
 	public class SqlDataGrid
 	{
 		private readonly SqlDatabase _sqlDatabase;
+		private readonly SqlDataTable _sqlDataTable;
 		private SqlDatabase SqlDatabase
 		{
 			get => _sqlDatabase;
 			init => _sqlDatabase = value;
 		}
+		private SqlDataTable SqlDataTable
+		{
+			get => _sqlDataTable;
+			init => _sqlDataTable = value;
+		}
 
 		public SqlDataGrid()
 		{
 			SqlDatabase = new SqlDatabase();
+			SqlDataTable = new SqlDataTable();
 		}
 		public void FillDataGridWithQuery(string query, DataGrid dataGrid)
 		{
