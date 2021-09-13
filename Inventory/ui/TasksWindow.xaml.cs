85dad5b8f8e001481a1ee5b88eed412526d0c41b
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-09-13 13:45:11 -0500

    Agregada propiedad CurrentTask

--- Inventory/ui/TasksWindow.xaml.cs
+++ Inventory/ui/TasksWindow.xaml.cs
@@ -4,6 +4,18 @@ namespace Inventory.ui
 {
 	public partial class TasksWindow : Window
 	{
+		private int _currentTask;
+		private int CurrentTask
+		{
+			get => _currentTask;
+			set {
+				if (value is >= 0 or <= 6)
+				{
+					_currentTask = value;
+				}
+			}
+	}
+		
 		public TasksWindow()
 		{
 			InitializeComponent();
