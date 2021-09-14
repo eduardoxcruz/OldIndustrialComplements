2fc532ad206aab9d3cef180f9bcd8a226a48861d
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-09-14 12:39:29 -0500

    Reformateado archivo

--- Inventory/model/Product.cs
+++ Inventory/model/Product.cs
@@ -12,62 +12,61 @@
 		public string ShortDescription { get; set; }
 		public string FullDescription { get; set; }
 		public bool TheProductUsesInventory { get; set; }
-        public string CurrentProductStock { get; set; }
-        public string MinProductStock { get; set; }
-        public string MaxProductStock { get; set; }
-        public string Container { get; set; }
-        public string Location { get; set; }
-        public string BranchOffice { get; set; }
-        public string Shelf { get; set; }
-        public string PurchasePrice { get; set; }
-        public string Unit { get; set; }
-        public string ProductType { get; set; }
-        public string Manufacturer { get; set; }
-        public string ManufacturerPartNumber { get; set; }
-        public bool ManualProfit { get; set; }
-        public string PercentageOfProfit { get; set; }
-        public string SalePrice { get; set; }
-        public string Utility { get; set; }
-        public string DiscountRate { get; set; }
-        public string PriceWithDiscount { get; set; }
-        public string ProfitWithDiscount { get; set; }
-        public string Memo { get; set; }
-        public Product()
-        {
-	        this.ProductId = 0;
-	        this.DebugCode = "";
-	        this.State = "";
-		    this.Enrollment = "";
-	        this.MountingTechnology = "";
-		    this.EncapsulationType = "";
-	        this.Category = "";
-		    this.ShortDescription = "";
-	        this.FullDescription = "";
-		    this.TheProductUsesInventory = false;
-	        this.CurrentProductStock = "";
-		    this.MinProductStock = "";
-	        this.MaxProductStock = "";
-		    this.Container = "";
-	        this.Location = "";
-		    this.BranchOffice = "";
-	        this.Shelf = "";
-		    this.PurchasePrice = "";
-	        this.Unit = "";
-		    this.ProductType = "";
-	        this.Manufacturer = "";
-		    this.ManufacturerPartNumber = "";
-	        this.ManualProfit = false;
-		    this.PercentageOfProfit = "";
-	        this.SalePrice = "";
-		    this.Utility = "";
-	        this.DiscountRate = "";
-		    this.PriceWithDiscount = "";
-	        this.ProfitWithDiscount = "";
-		    this.Memo = "";
-        }
-        public void GetDataFromSqlDatabase(string id)
-        {
-	        
-        }
+		public string CurrentProductStock { get; set; }
+		public string MinProductStock { get; set; }
+		public string MaxProductStock { get; set; }
+		public string Container { get; set; }
+		public string Location { get; set; }
+		public string BranchOffice { get; set; }
+		public string Shelf { get; set; }
+		public string PurchasePrice { get; set; }
+		public string Unit { get; set; }
+		public string ProductType { get; set; }
+		public string Manufacturer { get; set; }
+		public string ManufacturerPartNumber { get; set; }
+		public bool ManualProfit { get; set; }
+		public string PercentageOfProfit { get; set; }
+		public string SalePrice { get; set; }
+		public string Utility { get; set; }
+		public string DiscountRate { get; set; }
+		public string PriceWithDiscount { get; set; }
+		public string ProfitWithDiscount { get; set; }
+		public string Memo { get; set; }
+		public Product()
+		{
+			this.ProductId = 0;
+			this.DebugCode = "";
+			this.State = "";
+			this.Enrollment = "";
+			this.MountingTechnology = "";
+			this.EncapsulationType = "";
+			this.Category = "";
+			this.ShortDescription = "";
+			this.FullDescription = "";
+			this.TheProductUsesInventory = false;
+			this.CurrentProductStock = "";
+			this.MinProductStock = "";
+			this.MaxProductStock = "";
+			this.Container = "";
+			this.Location = "";
+			this.BranchOffice = "";
+			this.Shelf = "";
+			this.PurchasePrice = "";
+			this.Unit = "";
+			this.ProductType = "";
+			this.Manufacturer = "";
+			this.ManufacturerPartNumber = "";
+			this.ManualProfit = false;
+			this.PercentageOfProfit = "";
+			this.SalePrice = "";
+			this.Utility = "";
+			this.DiscountRate = "";
+			this.PriceWithDiscount = "";
+			this.ProfitWithDiscount = "";
+			this.Memo = "";
+		}
+		public void GetDataFromSqlDatabase(string id)
+		{
+		}
 	}
 }
