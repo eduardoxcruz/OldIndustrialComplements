d86317cd9f73c109c90ac02eca5a16ca17554905
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-11-01 15:24:11 -0600

    Eliminada propiedad SqlDatabase

--- Inventory/data/SqlDataGrid.cs
+++ Inventory/data/SqlDataGrid.cs
@@ -5,22 +5,10 @@ namespace Inventory.data
 {
 	public class SqlDataGrid
 	{
-		private readonly SqlDatabase _sqlDatabase;
-		private readonly SqlDataTable _sqlDataTable;
-		private SqlDatabase SqlDatabase
-		{
-			get => _sqlDatabase;
-			init => _sqlDatabase = value;
-		}
-		private SqlDataTable SqlDataTable
-		{
-			get => _sqlDataTable;
-			init => _sqlDataTable = value;
-		}
+		private SqlDataTable SqlDataTable { get; init; }
 
 		public SqlDataGrid()
 		{
-			SqlDatabase = new SqlDatabase();
 			SqlDataTable = new SqlDataTable();
 		}
 		public void FillDataGridWithQuery(string query, DataGrid dataGrid)
