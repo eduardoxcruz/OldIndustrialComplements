ddc397bb41890a5e0cf7f6972cfd02501b16ebde
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-11-05 15:11:31 -0600

    Extraidas propiedades y usadas las de Constant.Sql

--- Inventory/data/SqlDatabase.cs
+++ Inventory/data/SqlDatabase.cs
@@ -6,17 +6,6 @@ namespace Inventory.data
 {
 	public class SqlDatabase : IDisposable
 	{
-		private readonly string _serverIp = string.IsNullOrEmpty(Properties.Settings.Default.DatabaseIp) ? 
-			"192.168.0.254" : 
-			Properties.Settings.Default.DatabaseIp;
-		private const string ServerPort = "1433";
-		private const string DatabaseName = "pruebas";
-		private const string IntegratedSecurity = "False";
-		private const string UserId = "sa";
-		private const string Password = "Tlacua015";
-		private const string MultipleActiveResultSets = "True";
-		private const string ConnectionTimeoutInSeconds = "10";
-		
 		private string _databaseConnectionString;
 		private SqlConnection _databaseConnection;
 		private string DatabaseConnectionString
@@ -47,13 +36,13 @@ namespace Inventory.data
 		
 		public SqlDatabase()
 		{
-			DatabaseConnectionString = "Server=" + _serverIp + "," + ServerPort + 
-			                           ";Database=" + DatabaseName + 
-			                           ";Integrated Security=" + IntegratedSecurity + 
-			                           ";User Id=" + UserId + 
-			                           ";Password=" + Password + 
-			                           ";MultipleActiveResultSets=" + MultipleActiveResultSets + 
-			                           ";Connection Timeout=" + ConnectionTimeoutInSeconds;
+			DatabaseConnectionString = "Server=" + Sql.ServerIp + "," + Sql.ServerPort + 
+			                           ";Database=" + Sql.DatabaseName + 
+			                           ";Integrated Security=" + Sql.IntegratedSecurity + 
+			                           ";User Id=" + Sql.UserId + 
+			                           ";Password=" + Sql.Password + 
+			                           ";MultipleActiveResultSets=" + Sql.MultipleActiveResultSets + 
+			                           ";Connection Timeout=" + Sql.ConnectionTimeoutInSeconds;
 			DatabaseConnection = new SqlConnection(DatabaseConnectionString);
 		}
 		public SqlDataReader Read(string query)
