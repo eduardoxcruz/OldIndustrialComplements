daf5797f7743f7a0b2a14cd96336d34c7bcf4f0c
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-11-08 14:25:16 -0600

    Agregado

---
+++ Inventory/model/MyWindow.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace Inventory.model
+{
+	public class MyWindow : Window
+	{
+		public void BringWindowToFront()
+		{
+			if (this.Visibility == Visibility.Collapsed)
+			{
+				this.Show();
+			}
+
+			if (this.WindowState == WindowState.Minimized || this.Visibility == Visibility.Hidden)
+			{
+				this.Visibility = Visibility.Visible;
+				this.WindowState = WindowState.Normal;
+			}
+
+			this.Activate();
+		}
+		protected override void OnClosing(CancelEventArgs cancelEventArgs)
+		{
+			cancelEventArgs.Cancel = true;
+			this.Hide();
+		}
+
+		protected void CloseWindow(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
