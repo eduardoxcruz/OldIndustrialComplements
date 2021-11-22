beaafbc2e8273f7016273dee42cf54860d0433b8
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-11-22 16:23:44 -0600

    Agregado

---
+++ Inventory/model/ProductRequest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Inventory.model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+#nullable disable
+
+namespace MiPrimerEntityFramework.model
+{
+	public class ProductRequest
+	{
+		public int? Id { get; set; }
+		public DateTime? Date { get; set; }
+		public int? Amount { get; set; }
+		public string? EmployeeName { get; set; }
+
+		public Employee Employee { get; set; }
+		public int? EmployeeId { get; set; }
+		public Product Product { get; set; }
+		public int? ProductId { get; set; }
+	}
+
+	public class ProductRequestEntityTypeConfiguration : IEntityTypeConfiguration<ProductRequest>
+	{
+		public void Configure(EntityTypeBuilder<ProductRequest> builder)
+		{
+			builder.HasKey(productRequest => productRequest.Id);
+
+			builder
+				.Property(productRequest => productRequest.Id)
+				.ValueGeneratedNever();
+
+			builder.Property(productRequest => productRequest.Amount);
+
+			builder.Property(productRequest => productRequest.Date);
+
+			builder
+				.Property(productRequest => productRequest.EmployeeName)
+				.HasMaxLength(35)
+				.IsUnicode(false);
+		}
+	}
+}
