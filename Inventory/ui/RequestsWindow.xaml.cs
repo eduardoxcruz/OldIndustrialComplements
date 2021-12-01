2da819bde7346d00c4623eeaf25a643e3f9a3dd9
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-12-01 15:49:22 -0600

    Revert "Agregada propiedad productRequests"
    This reverts commit 6c17ce71c9afe312b68b7dea64bfe16ca8951e4c.

--- Inventory/ui/RequestsWindow.xaml.cs
+++ Inventory/ui/RequestsWindow.xaml.cs
@@ -1,11 +1,8 @@
-﻿using Inventory.model;
-
-namespace Inventory.ui
+﻿namespace Inventory.ui
 {
 	public partial class RequestsWindow
 	{
 		public static readonly RequestsWindow Instance = new();
-		private static ProductRequest productRequests { get; set; }
 		private RequestsWindow()
 		{
 			InitializeComponent();
