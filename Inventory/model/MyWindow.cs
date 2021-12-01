6619c63c7f9768292a90ee03e2a1451557ae31be
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-12-01 10:31:29 -0600

    Agregados metodos AllowOnlyNumbers y TextMatchNumbers

--- Inventory/model/MyWindow.cs
+++ Inventory/model/MyWindow.cs
@@ -1,10 +1,13 @@
 ﻿using System.ComponentModel;
+using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Inventory.model
 {
 	public class MyWindow : Window
 	{
+		private static readonly Regex Regex = new Regex("[^0-9]+");
 		public void BringWindowToFront()
 		{
 			if (this.Visibility == Visibility.Collapsed)
@@ -25,10 +28,17 @@ namespace Inventory.model
 			cancelEventArgs.Cancel = true;
 			this.Hide();
 		}
-
 		protected void CloseWindow(object sender, RoutedEventArgs e)
 		{
 			this.Close();
+		}
+		protected void AllowOnlyNumbers(object sender, TextCompositionEventArgs e)
+		{
+			e.Handled = TextMatchNumbers(e.Text);
+		}
+		private static bool TextMatchNumbers(string text)
+		{
+			return Regex.IsMatch(text);
 		}
 	}
 }
