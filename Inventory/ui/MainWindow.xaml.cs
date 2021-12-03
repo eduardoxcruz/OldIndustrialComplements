3c077a65a8fc2c06d42244390eed4bbcc73e5ccf
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-12-03 13:24:48 -0600

    Eliminados espacios

--- Inventory/ui/MainWindow.xaml.cs
+++ Inventory/ui/MainWindow.xaml.cs
@@ -11,32 +11,26 @@ namespace Inventory.ui
 		{
 			InitializeComponent();
 		}
-
 		private void BtnOpenTasksWindow_Click(object sender, RoutedEventArgs e)
 		{
 			TasksWindow.Instance.BringWindowToFront();
 		}
-
 		private void BtnOpenSearchProductWindow_Click(object sender, RoutedEventArgs e)
 		{
 			SearchProductWindow.Instance.BringWindowToFront();
 		}
-
 		private void BtnOpenConecctionsWindow_Click(object sender, RoutedEventArgs e)
 		{
 			ConnectionsWindow.Instance.BringWindowToFront();
 		}
-
 		private void BtnOpenSettingWindow_Click(object sender, RoutedEventArgs e)
 		{
 			SettingsWindow.Instance.BringWindowToFront();
 		}
-
 		private void BtnOpenModifyWindow_Click(object sender, RoutedEventArgs e)
 		{
 			ProductWindow.ModifyProductInstance.BringWindowToFront();
 		}
-
 		private void BtnOpenAddProductWindow_Click(object sender, RoutedEventArgs e)
 		{
 			ProductWindow.AddNewProductInstance.BringWindowToFront();
