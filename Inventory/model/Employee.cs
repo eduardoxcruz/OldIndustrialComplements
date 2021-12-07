5159a731a0830ac1ffc3d8d827672118fa19615c
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-12-07 10:14:12 -0600

    Corregida indentacion y espaciado

--- Inventory/model/Employee.cs
+++ Inventory/model/Employee.cs
@@ -7,107 +7,108 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Inventory.model
 {
-    public class Employee : INotifyPropertyChanged
-    {
-	    public event PropertyChangedEventHandler? PropertyChanged;
+	public class Employee : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler? PropertyChanged;
 
-	    private int _id;
-	    private string? _type;
-	    private string _fullName;
-	    private string? _password;
+		private int _id;
+		private string? _type;
+		private string _fullName;
+		private string? _password;
 
-	    public int Id
-	    {
-		    get
-		    {
-			    return _id;
-		    }
-		    set
-		    {
-			    _id = value;
-			    OnPropertyChanged(nameof(Id));
-		    }
-	    }
+		public int Id
+		{
+			get
+			{
+				return _id;
+			}
+			set
+			{
+				_id = value;
+				OnPropertyChanged(nameof(Id));
+			}
+		}
 
-	    public string? Type
-	    {
-		    get
-		    {
-			    return _type;
-		    }
-		    set
-		    {
-			    _type = value;
-			    OnPropertyChanged(nameof(Type));
-		    }
-	    }
+		public string? Type
+		{
+			get
+			{
+				return _type;
+			}
+			set
+			{
+				_type = value;
+				OnPropertyChanged(nameof(Type));
+			}
+		}
 
-	    public string FullName
-	    {
-		    get
-		    {
-			    return _fullName;
-		    }
-		    set
-		    {
-			    _fullName = value;
-			    OnPropertyChanged(nameof(FullName));
-		    }
-	    }
+		public string FullName
+		{
+			get
+			{
+				return _fullName;
+			}
+			set
+			{
+				_fullName = value;
+				OnPropertyChanged(nameof(FullName));
+			}
+		}
 
-	    public string? Password
-	    {
-		    get
-		    {
-			    return _password;
-		    }
-		    set
-		    {
-			    _password = value;
-			    OnPropertyChanged(nameof(Password));
-		    }
-	    }
+		public string? Password
+		{
+			get
+			{
+				return _password;
+			}
+			set
+			{
+				_password = value;
+				OnPropertyChanged(nameof(Password));
+			}
+		}
 
-	    public List<ProductForBuy> ProductForBuys { get; set; }
-        public List<ProductRequest> ProductRequests { get; set; }
-        public List<RecordOfProductMovement> RecordOfProductMovements { get; set; }
+		public List<ProductForBuy> ProductForBuys { get; set; }
+		public List<ProductRequest> ProductRequests { get; set; }
+		public List<RecordOfProductMovement> RecordOfProductMovements { get; set; }
 
-        public Employee()
-        { 
-	        this.Id = 0;
-	        this.Type = "";
-	        this.FullName = "";
-	        this.Password = "";
-        }
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-	        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
+		public Employee()
+		{
+			this.Id = 0;
+			this.Type = "";
+			this.FullName = "";
+			this.Password = "";
+		}
 
-    public class EmployeeEntityTypeConfiguration : IEntityTypeConfiguration<Employee>
-    {
-        public void Configure(EntityTypeBuilder<Employee> builder)
-        {
-            builder.HasKey(employee => employee.Id);
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
 
-            builder.Property(employee => employee.Id)
-                .ValueGeneratedOnAdd();
-            
-            builder
-                .Property(employee => employee.FullName)
-                .HasMaxLength(30)
-                .IsUnicode(false);
-            
-            builder
-                .Property(employee => employee.Password)
-                .HasMaxLength(10)
-                .IsUnicode(false);
+	public class EmployeeEntityTypeConfiguration : IEntityTypeConfiguration<Employee>
+	{
+		public void Configure(EntityTypeBuilder<Employee> builder)
+		{
+			builder.HasKey(employee => employee.Id);
 
-            builder
-                .Property(employee => employee.Type)
-                .HasMaxLength(20)
-                .IsUnicode(false);
-        }
-    }
+			builder.Property(employee => employee.Id)
+				.ValueGeneratedOnAdd();
+
+			builder
+				.Property(employee => employee.FullName)
+				.HasMaxLength(30)
+				.IsUnicode(false);
+
+			builder
+				.Property(employee => employee.Password)
+				.HasMaxLength(10)
+				.IsUnicode(false);
+
+			builder
+				.Property(employee => employee.Type)
+				.HasMaxLength(20)
+				.IsUnicode(false);
+		}
+	}
 }
