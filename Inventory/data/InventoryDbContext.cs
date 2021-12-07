847f86c2366629ed1c9d54f4b70f2927330dc15e
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-12-07 10:10:15 -0600

    Corregida indentacion

--- Inventory/data/InventoryDbContext.cs
+++ Inventory/data/InventoryDbContext.cs
@@ -8,166 +8,170 @@ using Microsoft.EntityFrameworkCore;
 namespace Inventory.data
 {
 	public delegate void DatabaseRequest();
-	
-    public class InventoryDbContext : DbContext
-    {
-        public virtual DbSet<ProductForBuy> ProductsForBuy { get; set; }
-        public virtual DbSet<RecordOfProductMovement> RecordsOfProductMovements { get; set; }
-        public virtual DbSet<Product> Products { get; set; }
-        public virtual DbSet<ProductRequest> ProductRequests { get; set; }
-        public virtual DbSet<Employee> Employees { get; set; }
-        
-        private const string ServerIp = "192.168.0.254";
-        private const string ServerPort = "1433";
-        private const string DatabaseName = "inventory";
-        private const string IntegratedSecurity = "False";
-        private const string UserId = "sa";
-        private const string Password = "Tlacua015";
-        private const string MultipleActiveResultSets = "True";
-        private const string ConnectionTimeoutInSeconds = "10";
-        private const string ConnectionString  = "Server=" + ServerIp + "," + ServerPort +
-                                                 ";Database=" + DatabaseName +
-                                                 ";Integrated Security=" + IntegratedSecurity +
-                                                 ";User Id=" + UserId +
-                                                 ";Password=" + Password +
-                                                 ";MultipleActiveResultSets=" + MultipleActiveResultSets +
-                                                 ";Connection Timeout=" + ConnectionTimeoutInSeconds;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        { 
-	        optionsBuilder.UseSqlServer(ConnectionString);
-        }
-        
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.HasAnnotation("Relational:Collation", "Modern_Spanish_CI_AS");
-            
-            new ProductForBuyEntityTypeConfiguration().Configure(modelBuilder.Entity<ProductForBuy>());
-            new RecordOfProductMovementEntityTypeConfiguration().Configure(modelBuilder.Entity<RecordOfProductMovement>());
-            new ProductEntityTypeConfiguration().Configure(modelBuilder.Entity<Product>());
-            new ProductRequestEntityTypeConfiguration().Configure(modelBuilder.Entity<ProductRequest>());
-            new EmployeeEntityTypeConfiguration().Configure(modelBuilder.Entity<Employee>());
+	public class InventoryDbContext : DbContext
+	{
+		public virtual DbSet<ProductForBuy> ProductsForBuy { get; set; }
+		public virtual DbSet<RecordOfProductMovement> RecordsOfProductMovements { get; set; }
+		public virtual DbSet<Product> Products { get; set; }
+		public virtual DbSet<ProductRequest> ProductRequests { get; set; }
+		public virtual DbSet<Employee> Employees { get; set; }
 
-            modelBuilder = ConfigureRelationships(modelBuilder);
-        }
-        private static ModelBuilder ConfigureRelationships(ModelBuilder modelBuilder)
-        {
-	        modelBuilder
-		        .Entity<ProductForBuy>()
-		        .HasOne(productForBuy => productForBuy.Product)
-		        .WithMany(product => product.ProductForBuys)
-		        .HasForeignKey(productForBuy => productForBuy.ProductId)
-		        .OnDelete(DeleteBehavior.Cascade);
-	        
-	        modelBuilder
-		        .Entity<ProductForBuy>()
-		        .HasOne(productForBuy => productForBuy.Employee)
-		        .WithMany(employee => employee.ProductForBuys)
-		        .HasForeignKey(productForBuy => productForBuy.EmployeeId)
-		        .OnDelete(DeleteBehavior.SetNull);
-	        
-	        modelBuilder
-		        .Entity<ProductRequest>()
-		        .HasOne(productRequest => productRequest.Product)
-		        .WithMany(product => product.ProductRequests)
-		        .HasForeignKey(productRequest => productRequest.ProductId)
-		        .OnDelete(DeleteBehavior.Cascade);
-	        
-	        modelBuilder
-		        .Entity<ProductRequest>()
-		        .HasOne(productRequest => productRequest.Employee)
-		        .WithMany(employee => employee.ProductRequests)
-		        .HasForeignKey(productRequest => productRequest.EmployeeId)
-		        .OnDelete(DeleteBehavior.SetNull);
-	        
-	        modelBuilder
-		        .Entity<RecordOfProductMovement>()
-		        .HasOne(recordOfProductMovement => recordOfProductMovement.Product)
-		        .WithMany(product => product.RecordOfProductMovements)
-		        .HasForeignKey(recordOfProductMovement => recordOfProductMovement.ProductId)
-		        .OnDelete(DeleteBehavior.Cascade);
-	        
-	        modelBuilder
-		        .Entity<RecordOfProductMovement>()
-		        .HasOne(recordOfProductMovement => recordOfProductMovement.Employee)
-		        .WithMany(employee => employee.RecordOfProductMovements)
-		        .HasForeignKey(recordOfProductMovement => recordOfProductMovement.EmployeeId)
-		        .OnDelete(DeleteBehavior.SetNull);
-	        
-	        modelBuilder
-                .Entity<Product>()
-                .Navigation(product => product.ProductForBuys)
-                .UsePropertyAccessMode(PropertyAccessMode.Property);
+		private const string ServerIp = "192.168.0.254";
+		private const string ServerPort = "1433";
+		private const string DatabaseName = "inventory";
+		private const string IntegratedSecurity = "False";
+		private const string UserId = "sa";
+		private const string Password = "Tlacua015";
+		private const string MultipleActiveResultSets = "True";
+		private const string ConnectionTimeoutInSeconds = "10";
 
-            modelBuilder
-                .Entity<Employee>()
-                .Navigation(employee => employee.ProductForBuys)
-                .UsePropertyAccessMode(PropertyAccessMode.Property);
+		private const string ConnectionString = "Server=" + ServerIp + "," + ServerPort +
+		                                        ";Database=" + DatabaseName +
+		                                        ";Integrated Security=" + IntegratedSecurity +
+		                                        ";User Id=" + UserId +
+		                                        ";Password=" + Password +
+		                                        ";MultipleActiveResultSets=" + MultipleActiveResultSets +
+		                                        ";Connection Timeout=" + ConnectionTimeoutInSeconds;
 
-            modelBuilder
-                .Entity<Product>()
-                .Navigation(product => product.ProductRequests)
-                .UsePropertyAccessMode(PropertyAccessMode.Property);
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(ConnectionString);
+		}
 
-            modelBuilder
-                .Entity<Employee>()
-                .Navigation(employee => employee.ProductRequests)
-                .UsePropertyAccessMode(PropertyAccessMode.Property);
-            
-            modelBuilder
-                .Entity<Product>()
-                .Navigation(product => product.RecordOfProductMovements)
-                .UsePropertyAccessMode(PropertyAccessMode.Property);
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.HasAnnotation("Relational:Collation", "Modern_Spanish_CI_AS");
 
-            modelBuilder
-                .Entity<Employee>()
-                .Navigation(employee => employee.RecordOfProductMovements)
-                .UsePropertyAccessMode(PropertyAccessMode.Property);
-            
-            modelBuilder
-                .Entity<ProductForBuy>()
-                .HasIndex(productForBuy => productForBuy.ProductId)
-                .IsUnique(false);
+			new ProductForBuyEntityTypeConfiguration().Configure(modelBuilder.Entity<ProductForBuy>());
+			new RecordOfProductMovementEntityTypeConfiguration().Configure(
+				modelBuilder.Entity<RecordOfProductMovement>());
+			new ProductEntityTypeConfiguration().Configure(modelBuilder.Entity<Product>());
+			new ProductRequestEntityTypeConfiguration().Configure(modelBuilder.Entity<ProductRequest>());
+			new EmployeeEntityTypeConfiguration().Configure(modelBuilder.Entity<Employee>());
 
-            modelBuilder
-                .Entity<ProductForBuy>()
-                .HasIndex(productForBuy => productForBuy.EmployeeId)
-                .IsUnique(false);
+			modelBuilder = ConfigureRelationships(modelBuilder);
+		}
 
-            modelBuilder
-                .Entity<ProductRequest>()
-                .HasIndex(productRequest => productRequest.ProductId)
-                .IsUnique(false);
+		private static ModelBuilder ConfigureRelationships(ModelBuilder modelBuilder)
+		{
+			modelBuilder
+				.Entity<ProductForBuy>()
+				.HasOne(productForBuy => productForBuy.Product)
+				.WithMany(product => product.ProductForBuys)
+				.HasForeignKey(productForBuy => productForBuy.ProductId)
+				.OnDelete(DeleteBehavior.Cascade);
 
-            modelBuilder
-                .Entity<ProductRequest>()
-                .HasIndex(productRequest => productRequest.EmployeeId)
-                .IsUnique(false);
+			modelBuilder
+				.Entity<ProductForBuy>()
+				.HasOne(productForBuy => productForBuy.Employee)
+				.WithMany(employee => employee.ProductForBuys)
+				.HasForeignKey(productForBuy => productForBuy.EmployeeId)
+				.OnDelete(DeleteBehavior.SetNull);
 
-            modelBuilder
-                .Entity<RecordOfProductMovement>()
-                .HasIndex(recordOfProductMovement => recordOfProductMovement.ProductId)
-                .IsUnique(false);
+			modelBuilder
+				.Entity<ProductRequest>()
+				.HasOne(productRequest => productRequest.Product)
+				.WithMany(product => product.ProductRequests)
+				.HasForeignKey(productRequest => productRequest.ProductId)
+				.OnDelete(DeleteBehavior.Cascade);
 
-            modelBuilder
-                .Entity<RecordOfProductMovement>()
-                .HasIndex(recordOfProductMovement => recordOfProductMovement.EmployeeId)
-                .IsUnique(false);
-            
-            return modelBuilder;
-        }
-        public static void ExecuteDatabaseRequest(DatabaseRequest request)
-        {
-	        try
-	        {
-		        request();
-	        }
-	        catch (Exception exception)
-	        {
-		        MessageBox.Show("Ha ocurrido un error al procesar su solicitud. Intentelo de nuevo.\nMas info: \n\n" 
-		                        + exception.Message,
-			        "Error");
-	        }
-        }
-    }
+			modelBuilder
+				.Entity<ProductRequest>()
+				.HasOne(productRequest => productRequest.Employee)
+				.WithMany(employee => employee.ProductRequests)
+				.HasForeignKey(productRequest => productRequest.EmployeeId)
+				.OnDelete(DeleteBehavior.SetNull);
+
+			modelBuilder
+				.Entity<RecordOfProductMovement>()
+				.HasOne(recordOfProductMovement => recordOfProductMovement.Product)
+				.WithMany(product => product.RecordOfProductMovements)
+				.HasForeignKey(recordOfProductMovement => recordOfProductMovement.ProductId)
+				.OnDelete(DeleteBehavior.Cascade);
+
+			modelBuilder
+				.Entity<RecordOfProductMovement>()
+				.HasOne(recordOfProductMovement => recordOfProductMovement.Employee)
+				.WithMany(employee => employee.RecordOfProductMovements)
+				.HasForeignKey(recordOfProductMovement => recordOfProductMovement.EmployeeId)
+				.OnDelete(DeleteBehavior.SetNull);
+
+			modelBuilder
+				.Entity<Product>()
+				.Navigation(product => product.ProductForBuys)
+				.UsePropertyAccessMode(PropertyAccessMode.Property);
+
+			modelBuilder
+				.Entity<Employee>()
+				.Navigation(employee => employee.ProductForBuys)
+				.UsePropertyAccessMode(PropertyAccessMode.Property);
+
+			modelBuilder
+				.Entity<Product>()
+				.Navigation(product => product.ProductRequests)
+				.UsePropertyAccessMode(PropertyAccessMode.Property);
+
+			modelBuilder
+				.Entity<Employee>()
+				.Navigation(employee => employee.ProductRequests)
+				.UsePropertyAccessMode(PropertyAccessMode.Property);
+
+			modelBuilder
+				.Entity<Product>()
+				.Navigation(product => product.RecordOfProductMovements)
+				.UsePropertyAccessMode(PropertyAccessMode.Property);
+
+			modelBuilder
+				.Entity<Employee>()
+				.Navigation(employee => employee.RecordOfProductMovements)
+				.UsePropertyAccessMode(PropertyAccessMode.Property);
+
+			modelBuilder
+				.Entity<ProductForBuy>()
+				.HasIndex(productForBuy => productForBuy.ProductId)
+				.IsUnique(false);
+
+			modelBuilder
+				.Entity<ProductForBuy>()
+				.HasIndex(productForBuy => productForBuy.EmployeeId)
+				.IsUnique(false);
+
+			modelBuilder
+				.Entity<ProductRequest>()
+				.HasIndex(productRequest => productRequest.ProductId)
+				.IsUnique(false);
+
+			modelBuilder
+				.Entity<ProductRequest>()
+				.HasIndex(productRequest => productRequest.EmployeeId)
+				.IsUnique(false);
+
+			modelBuilder
+				.Entity<RecordOfProductMovement>()
+				.HasIndex(recordOfProductMovement => recordOfProductMovement.ProductId)
+				.IsUnique(false);
+
+			modelBuilder
+				.Entity<RecordOfProductMovement>()
+				.HasIndex(recordOfProductMovement => recordOfProductMovement.EmployeeId)
+				.IsUnique(false);
+
+			return modelBuilder;
+		}
+
+		public static void ExecuteDatabaseRequest(DatabaseRequest request)
+		{
+			try
+			{
+				request();
+			}
+			catch (Exception exception)
+			{
+				MessageBox.Show("Ha ocurrido un error al procesar su solicitud. Intentelo de nuevo.\nMas info: \n\n"
+				                + exception.Message,
+					"Error");
+			}
+		}
+	}
 }
