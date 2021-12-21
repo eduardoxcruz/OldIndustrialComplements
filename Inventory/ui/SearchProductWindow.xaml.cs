25cd10fedb01e2a5a3adab60cc65a56b23344008
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-12-20 18:12:19 -0600

    Re diseñada interfaz y comportamiento

--- Inventory/ui/SearchProductWindow.xaml.cs
+++ Inventory/ui/SearchProductWindow.xaml.cs
@@ -1,7 +1,13 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 using Inventory.data;
 using Inventory.model;
 using Microsoft.EntityFrameworkCore;
@@ -11,86 +17,296 @@ namespace Inventory.ui
 	public partial class SearchProductWindow
 	{
 		public static readonly SearchProductWindow Instance = new();
+		private DispatcherTimer DispatcherTimer { get; set; }
+		private Product LastProduct { get; set; }
+		private InventoryDbContext InventoryDb { get; set; }
+		private ObservableCollection<Product> Products { get; set; }
+		private CollectionViewSource ProductsView { get; set; }
 
 		private SearchProductWindow()
 		{
 			InitializeComponent();
+			InventoryDb = new InventoryDbContext();
+			GetAllProducts();
+			StartDispatcherTimer();
+			ProductsView.Filter += MyProductsViewFilters;
 		}
 
+		private void GetAllProducts()
+		{
+			LastProduct = InventoryDb.Products.OrderByDescending(product => product.Id).FirstOrDefault() ??
+			              new Product();
+			InventoryDb.Products.Load();
+			Products = InventoryDb.Products.Local.ToObservableCollection();
+			ProductsView = new CollectionViewSource() {Source = Products};
+			ProductsView.SortDescriptions.Clear();
+			ProductsView.SortDescriptions.Add(new SortDescription("Id", ListSortDirection.Ascending));
+			DataGridProducts.ItemsSource = ProductsView.View;
+		}
+
+		private void StartDispatcherTimer()
+		{
+			DispatcherTimer = new DispatcherTimer {Interval = new TimeSpan(0, 0, 30)};
+			DispatcherTimer.Tick += AddNewProductToCollection;
+			DispatcherTimer.Start();
+		}
+		
+		private void AddNewProductToCollection(object sender, EventArgs e)
+		{
+			Product nextProduct = InventoryDb.Products.SingleOrDefault(product => product.Id == LastProduct.Id + 1);
+
+			if (nextProduct == null)
+			{
+				return;
+			}
+
+			if (Products.Count < nextProduct.Id)
+			{
+				LastProduct = nextProduct;
+				Products.Add(LastProduct);
+			}
+			
+			DataGridProducts.Items.Refresh();
+		}
+		
+		private void MyProductsViewFilters(object sender, FilterEventArgs filterEventArgs)
+		{
+			if (filterEventArgs.Item is not Product) return;
+
+			if (AllFiltersAreEmpty())
+			{
+				filterEventArgs.Accepted = true;
+				return;
+			}
+			
+			if (!string.IsNullOrEmpty(TxtBoxQuickSearch.Text))
+			{
+				QuickFilter(sender, filterEventArgs);
+				return;
+			}
+			
+			AdvancedFilter(sender, filterEventArgs);
+		}
+
+		private bool AllFiltersAreEmpty()
+		{
+			return string.IsNullOrEmpty(TxtBoxId.Text) &&
+			       string.IsNullOrEmpty(TxtBoxEnrollment.Text) &&
+			       string.IsNullOrEmpty(TxtBoxDescription.Text) &&
+			       string.IsNullOrEmpty(TxtBoxContainer.Text) &&
+			       string.IsNullOrEmpty(TxtBoxLocation.Text) &&
+			       string.IsNullOrEmpty(TxtBoxStatus.Text) &&
+			       string.IsNullOrEmpty(TxtBoxMountingTechnology.Text) &&
+			       string.IsNullOrEmpty(TxtBoxEncapsulation.Text) &&
+			       string.IsNullOrEmpty(TxtBoxDebugCode.Text) &&
+			       string.IsNullOrEmpty(TxtBoxQuickSearch.Text) &&
+			       string.IsNullOrEmpty(TxtBoxMinAmount.Text) &&
+			       string.IsNullOrEmpty(TxtBoxMaxAmount.Text);
+		}
+		
 		private void SearchAfterThreeCharacteres(object sender, TextChangedEventArgs e)
 		{
-			if (TxtBoxQuickSearch.Text.Length > 2)
+			if (TxtBoxQuickSearch.Text.Length <= 3)
 			{
-				QuickSearch(TxtBoxQuickSearch.Text);
+				return;
 			}
-		}
 
-		private void QuickSearch(string text)
+			ProductsView.View.Refresh();
+			LblResultCount.Content = "Recuento: " + DataGridProducts.Items.Count;
+		}
+		
+		private void QuickFilter(object sender, FilterEventArgs filterEventArgs)
 		{
-			using InventoryDbContext inventoryDb = new InventoryDbContext();
-			DataGridProducts.ItemsSource =
-				(from product in inventoryDb.Products
-					where
-						EF.Functions.Like(product.Id.ToString(), text) ||
-						EF.Functions.Like(product.DebugCode, "%" + text + "%") ||
-						EF.Functions.Like(product.Status, "%" + text + "%") ||
-						EF.Functions.Like(product.Enrollment, "%" + text + "%") ||
-						EF.Functions.Like(product.MountingTechnology, "%" + text + "%") ||
-						EF.Functions.Like(product.EncapsulationType, "%" + text + "%") ||
-						EF.Functions.Like(product.ShortDescription, "%" + text + "%") ||
-						EF.Functions.Like(product.Category, "%" + text + "%") ||
-						EF.Functions.Like(product.Container, "%" + text + "%") ||
-						EF.Functions.Like(product.Location, "%" + text + "%") ||
-						EF.Functions.Like(product.Manufacturer, "%" + text + "%") ||
-						EF.Functions.Like(product.PartNumber, "%" + text + "%") ||
-						EF.Functions.Like(product.TypeOfStock, "%" + text + "%") ||
-						EF.Functions.Like(product.Location, "%" + text + "%")
-					select product)
-				.ToList();
-			LblResultCount.Content = "Recuento: " + DataGridProducts.Items.Count.ToString();
+			Product product = (Product)filterEventArgs.Item;
+			string text = TxtBoxQuickSearch.Text.ToLower();
+
+			if (product.Id.ToString().Contains(text))
+			{
+				filterEventArgs.Accepted = true;
+				return;
+			}
+
+			if (product.DebugCode != null && product.DebugCode.ToLower().Contains(text))
+			{
+				filterEventArgs.Accepted = true;
+				return;
+			}
+
+			if (product.Status != null && product.Status.ToLower().Contains(text))
+			{
+				filterEventArgs.Accepted = true;
+				return;
+			}
+
+			if (product.Enrollment != null && product.Enrollment.ToLower().Contains(text))
+			{
+				filterEventArgs.Accepted = true;
+				return;
+			}
+
+			if (product.MountingTechnology != null && product.MountingTechnology.ToLower().Contains(text))
+			{
+				filterEventArgs.Accepted = true;
+				return;
+			}
+
+			if (product.EncapsulationType != null && product.EncapsulationType.ToLower().Contains(text))
+			{
+				filterEventArgs.Accepted = true;
+				return;
+			}
+
+			if (product.ShortDescription != null && product.ShortDescription.ToLower().Contains(text))
+			{
+				filterEventArgs.Accepted = true;
+				return;
+			}
+
+			if (product.Category != null && product.Category.ToLower().Contains(text))
+			{
+				filterEventArgs.Accepted = true;
+				return;
+			}
+
+			if (product.Container != null && product.Container.ToLower().Contains(text))
+			{
+				filterEventArgs.Accepted = true;
+				return;
+			}
+
+			if (product.Location != null && product.Location.ToLower().Contains(text))
+			{
+				filterEventArgs.Accepted = true;
+				return;
+			}
+
+			if (product.Manufacturer != null && product.Manufacturer.ToLower().Contains(text))
+			{
+				filterEventArgs.Accepted = true;
+				return;
+			}
+
+			if (product.PartNumber != null && product.PartNumber.ToLower().Contains(text))
+			{
+				filterEventArgs.Accepted = true;
+				return;
+			}
+
+			if (product.TypeOfStock != null && product.TypeOfStock.ToLower().Contains(text))
+			{
+				filterEventArgs.Accepted = true;
+				return;
+			}
+
+			if (product.Location != null && product.Location.ToLower().Contains(text))
+			{
+				filterEventArgs.Accepted = true;
+				return;
+			}
+			
+			filterEventArgs.Accepted = false;
 		}
 
 		private void EnterKeyPressed(object sender, KeyEventArgs e)
 		{
-			if (e.Key == Key.Enter)
+			if (e.Key != Key.Enter)
 			{
-				SearchWithFilters(sender, e);
+				return;
 			}
+			
+			SearchWithAdvancedFilters(null, null);
 		}
-
-		private void SearchWithFilters(object sender, RoutedEventArgs e)
+		
+		private void SearchWithAdvancedFilters(object sender, RoutedEventArgs e)
 		{
-			if (string.IsNullOrEmpty(TxtBoxId.Text) &
-			    string.IsNullOrEmpty(TxtBoxStatus.Text) &
-			    string.IsNullOrEmpty(TxtBoxEnrollment.Text) &
-			    string.IsNullOrEmpty(TxtBoxDescription.Text) &
-			    string.IsNullOrEmpty(TxtBoxMountingTechnology.Text) &
-			    string.IsNullOrEmpty(TxtBoxEncapsulation.Text) &
-			    string.IsNullOrEmpty(TxtBoxContainer.Text) &
-			    string.IsNullOrEmpty(TxtBoxLocation.Text) &
-			    string.IsNullOrEmpty(TxtBoxDebugCode.Text))
+			if (string.IsNullOrEmpty(TxtBoxId.Text) &&
+			    string.IsNullOrEmpty(TxtBoxStatus.Text) &&
+			    string.IsNullOrEmpty(TxtBoxEnrollment.Text) &&
+			    string.IsNullOrEmpty(TxtBoxDescription.Text) &&
+			    string.IsNullOrEmpty(TxtBoxMountingTechnology.Text) &&
+			    string.IsNullOrEmpty(TxtBoxEncapsulation.Text) &&
+			    string.IsNullOrEmpty(TxtBoxContainer.Text) &&
+			    string.IsNullOrEmpty(TxtBoxLocation.Text) &&
+			    string.IsNullOrEmpty(TxtBoxDebugCode.Text) &&
+			    string.IsNullOrEmpty(TxtBoxMinAmount.Text) &&
+			    string.IsNullOrEmpty(TxtBoxMaxAmount.Text))
 			{
 				MessageBox.Show("Llene al menos un campo para buscar.");
 				return;
 			}
 
-			using InventoryDbContext inventoryDb = new InventoryDbContext();
+			TxtBoxQuickSearch.Text = "";
+			ProductsView.View.Refresh();
+			LblResultCount.Content = "Recuento: " + DataGridProducts.Items.Count;
+		}
+		
+		private void AdvancedFilter(object sender, FilterEventArgs filterEventArgs)
+		{
+			Product product = (Product)filterEventArgs.Item;
 
-			DataGridProducts.ItemsSource =
-				(from product in inventoryDb.Products
-					where
-						EF.Functions.Like(product.Id.ToString(), "%" + TxtBoxId.Text + "%") &&
-						EF.Functions.Like(product.Enrollment, "%" + TxtBoxEnrollment.Text + "%") &&
-						EF.Functions.Like(product.ShortDescription, "%" + TxtBoxDescription.Text + "%") &&
-						EF.Functions.Like(product.Container, "%" + TxtBoxContainer.Text + "%") &&
-						EF.Functions.Like(product.Location, "%" + TxtBoxLocation.Text + "%") &&
-						EF.Functions.Like(product.Status, "%" + TxtBoxStatus.Text + "%") &&
-						EF.Functions.Like(product.MountingTechnology, "%" + TxtBoxMountingTechnology.Text + "%") &&
-						EF.Functions.Like(product.EncapsulationType, "%" + TxtBoxEncapsulation.Text + "%") &&
-						EF.Functions.Like(product.DebugCode, "%" + TxtBoxDebugCode.Text + "%")
-					select product).ToList();
+			if (product.Id.ToString().Contains(TxtBoxId.Text) &&
+				product.Enrollment.ToLower().Contains(TxtBoxEnrollment.Text.ToLower()) &&
+				product.ShortDescription.ToLower().Contains(TxtBoxDescription.Text.ToLower()) &&
+				product.Container.ToLower().Contains(TxtBoxContainer.Text.ToLower()) &&
+				product.Location.ToLower().Contains(TxtBoxLocation.Text.ToLower()) &&
+				product.Status.ToLower().Contains(TxtBoxStatus.Text.ToLower()) &&
+				product.MountingTechnology.ToLower().Contains(TxtBoxMountingTechnology.Text.ToLower()) &&
+				product.EncapsulationType.ToLower().Contains(TxtBoxEncapsulation.Text.ToLower()) &&
+				product.DebugCode.ToLower().Contains(TxtBoxDebugCode.Text.ToLower()) &&
+				IsValidAmount(filterEventArgs)
+				)
+			{
+				filterEventArgs.Accepted = true; 
+			}
 
-			LblResultCount.Content = "Recuento: " + DataGridProducts.Items.Count.ToString();
+			else filterEventArgs.Accepted = false;
+		}
+
+		private bool IsValidAmount(FilterEventArgs filterEventArgs)
+		{
+			if (string.IsNullOrEmpty(TxtBoxMinAmount.Text) && string.IsNullOrEmpty(TxtBoxMaxAmount.Text)) return true;
+			
+			Product product = (Product)filterEventArgs.Item;
+			int? minAmount = string.IsNullOrEmpty(TxtBoxMinAmount.Text) ? null : int.Parse(TxtBoxMinAmount.Text);
+			int? maxAmount = string.IsNullOrEmpty(TxtBoxMaxAmount.Text) ? null : int.Parse(TxtBoxMaxAmount.Text);
+
+			if (minAmount != null && maxAmount != null)
+			{
+
+				if (minAmount > maxAmount) return false;
+				
+				return product.CurrentAmount >= minAmount && product.CurrentAmount <= maxAmount;
+			}
+
+			if (minAmount != null)
+			{
+				return product.CurrentAmount >= minAmount;
+			}
+
+			if (maxAmount != null)
+			{
+				return product.CurrentAmount <= maxAmount;
+			}
+
+			return false;
+		}
+		
+		private void ClearFilters(object sender, RoutedEventArgs e)
+		{
+			TxtBoxId.Text = "";
+			TxtBoxStatus.Text = "";
+			TxtBoxEnrollment.Text = "";
+			TxtBoxDescription.Text = "";
+			TxtBoxMountingTechnology.Text = "";
+			TxtBoxEncapsulation.Text = "";
+			TxtBoxContainer.Text = "";
+			TxtBoxLocation.Text = "";
+			TxtBoxQuickSearch.Text = "";
+			TxtBoxDebugCode.Text = "";
+			TxtBoxMinAmount.Text = "";
+			TxtBoxMaxAmount.Text = "";
+			LblResultCount.Content = "Recuento: 00";
+			ProductsView.View.Refresh();
 		}
 
 		private void SelectProductFromDataGrid(object sender, MouseButtonEventArgs e)
@@ -100,35 +316,57 @@ namespace Inventory.ui
 				return;
 			}
 
-			Product selectedProduct = (Product)DataGridProducts.SelectedItems[0];
+			Product selectedProduct = ((sender as DataGridCell).DataContext as Product);
 			ProductWindow.ShowProductDetailsInstance.BringWindowToFront(selectedProduct);
 		}
 
-		private void CleanFilters(object sender, RoutedEventArgs e)
-		{
-			DataGridProducts.ItemsSource = null;
-			TxtBoxId.Text = null;
-			TxtBoxStatus.Text = null;
-			TxtBoxEnrollment.Text = null;
-			TxtBoxDescription.Text = null;
-			TxtBoxMountingTechnology.Text = null;
-			TxtBoxEncapsulation.Text = null;
-			TxtBoxContainer.Text = null;
-			TxtBoxLocation.Text = null;
-			TxtBoxQuickSearch.Text = null;
-			TxtBoxDebugCode.Text = null;
-			LblResultCount.Content = "Recuento: 00";
-		}
-
-		private void AddProductToSeparateDataGrid(object sender, RoutedEventArgs e)
+		private void AddProductToFilteredProducts(object sender, RoutedEventArgs e)
 		{
 			if (DataGridProducts.ItemsSource == null || DataGridProducts.SelectedItems.Count <= 0)
 			{
 				return;
 			}
 
-			Product selectedProduct = (Product)DataGridProducts.SelectedItems[0];
-			DataGridAddedProducts.Items.Add(selectedProduct);
+			foreach (Product selectedProduct in DataGridProducts.SelectedItems)
+				DataGridFilteredProducts.Items.Add(selectedProduct);
+		}
+
+		private void ClearDataGridFilteredProducts(object sender, RoutedEventArgs e)
+		{
+			DataGridFilteredProducts.Items.Clear();
+		}
+
+		private void DataGridKeyPressed(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Down || e.Key == Key.Up)
+				LoadProductImage(sender);
+		}
+		
+		private void MouseLeftButtonClick(object sender, MouseButtonEventArgs e)
+		{
+			LoadProductImage(sender);
+		}
+		
+		private void LoadProductImage(object sender)
+		{
+			if (DataGridProducts.ItemsSource == null || DataGridProducts.SelectedItems.Count <= 0)
+			{
+				return;
+			}
+			
+			Uri imageUri = null;
+			Product product = ((sender as DataGridCell).DataContext as Product);
+
+			try
+			{
+				imageUri = new Uri(Properties.Settings.Default.PhotosPath + "\\" + product.Id + ".jpg");
+				ProductImage.Source = new BitmapImage(imageUri);
+			}
+			catch (Exception exception)
+			{
+				imageUri = new Uri("/resources/images/defaultProductImage.jpg", UriKind.Relative);
+				ProductImage.Source = new BitmapImage(imageUri);
+			}
 		}
 
 		private void IngressProduct(object sender, RoutedEventArgs e)
@@ -140,28 +378,6 @@ namespace Inventory.ui
 
 			Product selectedProduct = (Product)DataGridProducts.SelectedItems[0];
 			TasksWindow.Instance.BringWindowToFront(selectedProduct, "ENTRADA DE PRODUCTO");
-		}
-
-		private void EgressProduct(object sender, RoutedEventArgs e)
-		{
-			if (DataGridProducts.ItemsSource == null || DataGridProducts.SelectedItems.Count <= 0)
-			{
-				return;
-			}
-
-			Product selectedProduct = (Product)DataGridProducts.SelectedItems[0];
-			TasksWindow.Instance.BringWindowToFront(selectedProduct, "SALIDA DE PRODUCTO");
-		}
-
-		private void ReturnProduct(object sender, RoutedEventArgs e)
-		{
-			if (DataGridProducts.ItemsSource == null || DataGridProducts.SelectedItems.Count <= 0)
-			{
-				return;
-			}
-
-			Product selectedProduct = (Product)DataGridProducts.SelectedItems[0];
-			TasksWindow.Instance.BringWindowToFront(selectedProduct, "DEVOLUCION DE PRODUCTO");
 		}
 
 		private void AdjustStock(object sender, RoutedEventArgs e)
@@ -228,11 +444,6 @@ namespace Inventory.ui
 
 			Product selectedProduct = (Product)DataGridProducts.SelectedItems[0];
 			TasksWindow.Instance.BringWindowToFront(selectedProduct, "SOLICITAR PARA VERIFICAR");
-		}
-
-		private void CleanOtherDataGrid(object sender, RoutedEventArgs e)
-		{
-			DataGridAddedProducts.Items.Clear();
 		}
 	}
 }
