e0ac051e78ff041aa47e6b4e13948856d00d8ac8
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2022-01-07 12:38:57 -0600

    Agregadas propiedades de la clase

--- Inventory/ui/ShoppingCartWindow.xaml.cs
+++ Inventory/ui/ShoppingCartWindow.xaml.cs
@@ -1,7 +1,18 @@
-﻿namespace Inventory.ui
+﻿using System.Collections.ObjectModel;
+using System.Windows.Data;
+using System.Windows.Threading;
+using Inventory.data;
+using Inventory.model;
+
+namespace Inventory.ui
 {
 	public partial class ShoppingCartWindow
 	{
+		public static readonly ShoppingCartWindow Instance = new();
+		private DispatcherTimer NewProductToBuyLookupTimer { get; set; }
+		private InventoryDbContext InventoryDb { get; set; }
+		private ObservableCollection<ProductForBuy> ProductsToBuyCollection { get; set; }
+		private CollectionViewSource ProductsToBuyView { get; set; }
 		public ShoppingCartWindow()
 		{
 			InitializeComponent();
