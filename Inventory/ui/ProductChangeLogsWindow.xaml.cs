77b0173d309631b6a767476ea031283ed085f3dc
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2022-01-11 12:58:27 -0600

    Agregada propiedad Instance

--- Inventory/ui/ProductChangeLogsWindow.xaml.cs
+++ Inventory/ui/ProductChangeLogsWindow.xaml.cs
@@ -2,6 +2,8 @@
 {
 	public partial class ProductChangeLogsWindow
 	{
+		public static readonly ProductChangeLogsWindow Instance = new();
+		
 		public ProductChangeLogsWindow()
 		{
 			InitializeComponent();
