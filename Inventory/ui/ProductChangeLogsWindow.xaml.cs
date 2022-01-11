7d22a0550dd9ed2a671b68370138b0b7b81f88a8
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2022-01-11 12:25:14 -0600

    Agregado

---
+++ Inventory/ui/ProductChangeLogsWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Inventory.ui
+{
+	/// <summary>
+	/// Lógica de interacción para ProductChangeLogsWindow.xaml
+	/// </summary>
+	public partial class ProductChangeLogsWindow : Window
+	{
+		public ProductChangeLogsWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
