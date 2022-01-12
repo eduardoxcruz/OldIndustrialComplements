47223ec18595ef5ce458701bac2f85520f469d3b
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2022-01-12 13:49:29 -0600

    Agregada implementacion de INotifyPropertyChanged

--- Inventory/model/ProductChangeCount.cs
+++ Inventory/model/ProductChangeCount.cs
@@ -1,7 +1,14 @@
-﻿namespace Inventory.model
+﻿using System.ComponentModel;
+
+namespace Inventory.model
 {
-	public class ProductChangeCount
+	public class ProductChangeCount : INotifyPropertyChanged
 	{
-		
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
 	}
 }
