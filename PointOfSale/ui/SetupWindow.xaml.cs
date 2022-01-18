962bc4fd76f5ca355afd2cd58dce0e2188555f68
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2022-01-18 11:49:28 -0600

    Agregado

---
+++ PointOfSale/ui/SetupWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace PointOfSale.ui
+{
+    /// <summary>
+    /// Lógica de interacción para SetupWindow.xaml
+    /// </summary>
+    public partial class SetupWindow : Window
+    {
+        public SetupWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
