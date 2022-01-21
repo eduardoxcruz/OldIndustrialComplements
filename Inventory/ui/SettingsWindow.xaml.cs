89bc90f2b1b121e095c9ef4bf97af5b688998ce5
Etienne <hernandezordaze@gmail.com>
2022-01-21 12:06:06 -0600

    Eliminado codicional if para obligar a llenar los campos en la ventana de configuración

--- Inventory/ui/SettingsWindow.xaml.cs
+++ Inventory/ui/SettingsWindow.xaml.cs
@@ -13,13 +13,6 @@ namespace Inventory.ui
 
 		private void SaveDatabaseIpAndPhotosPath(object sender, RoutedEventArgs e)
 		{
-			if (string.IsNullOrEmpty(TxtBoxIpAdress.Text) || string.IsNullOrEmpty(TxtxBoxSystemAdress.Text))
-			{
-				MessageBox.Show("Ningun campo puede quedar vacio", "Datos Invalidos", MessageBoxButton.OK,
-					MessageBoxImage.Exclamation);
-				return;
-			}
-
 			Properties.Settings.Default.DatabaseIp = TxtBoxIpAdress.Text;
 			Properties.Settings.Default.PhotosPath = TxtxBoxSystemAdress.Text;
 			Properties.Settings.Default.Save();
