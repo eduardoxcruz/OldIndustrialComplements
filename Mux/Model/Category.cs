045f4fe64abff68a1ef7be759456cbd087c70885
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-15 11:07:35 -0600

    Add INotifyPropertyChanged to Category class

--- Mux/Model/Category.cs
+++ Mux/Model/Category.cs
@@ -1,7 +1,15 @@
-﻿namespace Mux.Model
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Mux.Model
 {
-	public class Category
+	public class Category : INotifyPropertyChanged
 	{
-		
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
 	}
 }
