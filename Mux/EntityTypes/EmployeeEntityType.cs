4049dfef4e0ffd2d1b61adbc085786ac962790b5
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-16 16:09:12 -0600

    Add EntityTypeConfiguration implementation

--- Mux/EntityTypes/EmployeeEntityType.cs
+++ Mux/EntityTypes/EmployeeEntityType.cs
@@ -1,7 +1,32 @@
-﻿namespace Mux.EntityTypes
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Mux.Model;
+
+namespace Mux.EntityTypes
 {
-	internal class EmployeeEntityType
+	internal class EmployeeEntityType : IEntityTypeConfiguration<Employee>
 	{
-		
+		public void Configure(EntityTypeBuilder<Employee> builder)
+		{
+			builder.HasKey(employee => employee.Id);
+
+			builder.Property(employee => employee.Id)
+				.ValueGeneratedOnAdd();
+
+			builder
+				.Property(employee => employee.FullName)
+				.HasMaxLength(30)
+				.IsUnicode(false);
+
+			builder
+				.Property(employee => employee.Password)
+				.HasMaxLength(10)
+				.IsUnicode(false);
+
+			builder
+				.Property(employee => employee.Type)
+				.HasMaxLength(20)
+				.IsUnicode(false);
+		}
 	}
 }
