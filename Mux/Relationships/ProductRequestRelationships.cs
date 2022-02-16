59fc7046e39a8378769fa7864f771d3960741592
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-16 15:32:22 -0600

    Add RelationshipsConfiguration implementation

--- Mux/Relationships/ProductRequestRelationships.cs
+++ Mux/Relationships/ProductRequestRelationships.cs
@@ -1,7 +1,25 @@
-﻿namespace Mux.Relationships
+﻿using Microsoft.EntityFrameworkCore;
+using Mux.Model;
+
+namespace Mux.Relationships
 {
-	internal class ProductRequestRelationships
+	internal class ProductRequestRelationships : RelationshipsConfiguration
 	{
-		
+		public void Configure(ref ModelBuilder modelBuilder)
+		{
+			modelBuilder
+				.Entity<ProductRequest>()
+				.HasOne(productRequest => productRequest.Product)
+				.WithMany(product => product.ProductRequests)
+				.HasForeignKey(productRequest => productRequest.ProductId)
+				.OnDelete(DeleteBehavior.Cascade);
+
+			modelBuilder
+				.Entity<ProductRequest>()
+				.HasOne(productRequest => productRequest.Employee)
+				.WithMany(employee => employee.ProductRequests)
+				.HasForeignKey(productRequest => productRequest.EmployeeId)
+				.OnDelete(DeleteBehavior.SetNull);
+		}
 	}
 }
