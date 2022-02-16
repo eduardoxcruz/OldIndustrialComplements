509f2774613538d3c3e2ff741c7bd104c2febc88
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-16 15:01:32 -0600

    Add IndexPropertiesConfiguration implementation

--- Mux/IndexProperties/ProductToBuyIndexProperties.cs
+++ Mux/IndexProperties/ProductToBuyIndexProperties.cs
@@ -1,7 +1,21 @@
-﻿namespace Mux.IndexProperties
+﻿using Microsoft.EntityFrameworkCore;
+using Mux.Model;
+
+namespace Mux.IndexProperties
 {
-	internal class ProductToBuyIndexProperties
+	internal class ProductToBuyIndexProperties : IndexPropertiesConfiguration
 	{
-		
+		public void Configure(ref ModelBuilder modelBuilder)
+		{
+			modelBuilder
+				.Entity<ProductToBuy>()
+				.HasIndex(productForBuy => productForBuy.ProductId)
+				.IsUnique(false);
+
+			modelBuilder
+				.Entity<ProductToBuy>()
+				.HasIndex(productForBuy => productForBuy.EmployeeId)
+				.IsUnique(false);
+		}
 	}
 }
