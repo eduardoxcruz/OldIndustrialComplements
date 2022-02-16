be5880f5e1d46c61374f4da862e17dd4ad016cf3
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-16 15:30:45 -0600

    Add RelationshipsConfiguration implementation

--- Mux/Relationships/ShoppingCartRelationships.cs
+++ Mux/Relationships/ShoppingCartRelationships.cs
@@ -1,7 +1,25 @@
-﻿namespace Mux.Relationships
+﻿using Microsoft.EntityFrameworkCore;
+using Mux.Model;
+
+namespace Mux.Relationships
 {
-	internal class ShoppingCartRelationships
+	internal class ShoppingCartRelationships : RelationshipsConfiguration
 	{
-		
+		public void Configure(ref ModelBuilder modelBuilder)
+		{
+			modelBuilder
+				.Entity<ProductToBuy>()
+				.HasOne(productForBuy => productForBuy.Product)
+				.WithMany(product => product.ShoppingCart)
+				.HasForeignKey(productForBuy => productForBuy.ProductId)
+				.OnDelete(DeleteBehavior.Cascade);
+
+			modelBuilder
+				.Entity<ProductToBuy>()
+				.HasOne(productForBuy => productForBuy.Employee)
+				.WithMany(employee => employee.ShoppingCart)
+				.HasForeignKey(productForBuy => productForBuy.EmployeeId)
+				.OnDelete(DeleteBehavior.SetNull);
+		}
 	}
 }
