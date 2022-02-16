c29a9a163059f205b4ad28ed15f8cc1bd1c9e3c2
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-16 16:09:42 -0600

    Delete EmployeeEntityTypeConfiguration class

--- Mux/Model/Employee.cs
+++ Mux/Model/Employee.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Mux.Model
 {
@@ -67,32 +65,6 @@ namespace Mux.Model
 		protected virtual void OnPropertyChanged(string propertyName)
 		{
 			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-		}
-	}
-
-	public class EmployeeEntityTypeConfiguration : IEntityTypeConfiguration<Employee>
-	{
-		public void Configure(EntityTypeBuilder<Employee> builder)
-		{
-			builder.HasKey(employee => employee.Id);
-
-			builder.Property(employee => employee.Id)
-				.ValueGeneratedOnAdd();
-
-			builder
-				.Property(employee => employee.FullName)
-				.HasMaxLength(30)
-				.IsUnicode(false);
-
-			builder
-				.Property(employee => employee.Password)
-				.HasMaxLength(10)
-				.IsUnicode(false);
-
-			builder
-				.Property(employee => employee.Type)
-				.HasMaxLength(20)
-				.IsUnicode(false);
 		}
 	}
 }
