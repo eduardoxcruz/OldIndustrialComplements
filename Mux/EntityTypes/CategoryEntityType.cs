74883123150c11dfdff21c148f3730c9f1532fb1
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-16 16:06:48 -0600

    Add EntityTypeConfiguration implementation

--- Mux/EntityTypes/CategoryEntityType.cs
+++ Mux/EntityTypes/CategoryEntityType.cs
@@ -1,7 +1,23 @@
-﻿namespace Mux.EntityTypes
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Mux.Model;
+
+namespace Mux.EntityTypes
 {
-	internal class CategoryEntityType
+	internal class CategoryEntityType : IEntityTypeConfiguration<Category>
 	{
-		
+		public void Configure(EntityTypeBuilder<Category> builder)
+		{
+			builder.HasKey(category => category.Id);
+
+			builder
+				.Property(category => category.Id)
+				.ValueGeneratedOnAdd();
+
+			builder
+				.Property(category => category.Name)
+				.HasMaxLength(50)
+				.IsUnicode();
+		}
 	}
 }
