018b167a0b97bf7e0151c13b1e4b2e8091c7175c
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-16 16:19:24 -0600

    Add EntityTypeConfiguration implementation

--- Mux/EntityTypes/ProductToBuyEntityType.cs
+++ Mux/EntityTypes/ProductToBuyEntityType.cs
@@ -1,7 +1,37 @@
-﻿namespace Mux.EntityTypes
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Mux.Model;
+
+namespace Mux.EntityTypes
 {
-	internal class ProductToBuyEntityType
+	internal class ProductToBuyEntityType : IEntityTypeConfiguration<ProductToBuy>
 	{
-		
+		public void Configure(EntityTypeBuilder<ProductToBuy> builder)
+		{
+			builder.HasKey(productForBuy => productForBuy.Id);
+
+			builder
+				.Property(productForBuy => productForBuy.Id)
+				.ValueGeneratedNever();
+
+			builder.Property(productForBuy => productForBuy.RequestedAmount);
+
+			builder.Property(productForBuy => productForBuy.Date);
+
+			builder
+				.Property(productForBuy => productForBuy.EmployeeName)
+				.HasMaxLength(35)
+				.IsUnicode(false);
+
+			builder
+				.Property(productForBuy => productForBuy.Provider)
+				.HasMaxLength(35)
+				.IsUnicode(false);
+
+			builder
+				.Property(productForBuy => productForBuy.Status)
+				.HasMaxLength(10)
+				.IsUnicode(false);
+		}
 	}
 }
