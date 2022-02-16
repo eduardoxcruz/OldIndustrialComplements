627ffe592d0cd7a4a7931690808e7411ecdc2254
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-16 15:25:06 -0600

    Add Configure(ModelBuilder) method

--- Mux/Relationships/RelationshipsConfiguration.cs
+++ Mux/Relationships/RelationshipsConfiguration.cs
@@ -1,7 +1,9 @@
-﻿namespace Mux.Relationships
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mux.Relationships
 {
 	internal interface RelationshipsConfiguration
 	{
-		
+		void Configure(ref ModelBuilder modelBuilder);
 	}
 }
