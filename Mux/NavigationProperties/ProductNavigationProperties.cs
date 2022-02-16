bb5ef8198f938fa61bbfe9f212fadf12324aa847
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-16 15:15:42 -0600

    Add NavigationPropertiesConfiguration implementation

--- Mux/NavigationProperties/ProductNavigationProperties.cs
+++ Mux/NavigationProperties/ProductNavigationProperties.cs
@@ -1,7 +1,26 @@
-﻿namespace Mux.NavigationProperties
+﻿using Microsoft.EntityFrameworkCore;
+using Mux.Model;
+
+namespace Mux.NavigationProperties
 {
-	public class ProductNavigationProperties
+	public class ProductNavigationProperties : NavigationPropertiesConfiguration
 	{
-		
+		public void Configure(ref ModelBuilder modelBuilder)
+		{
+			modelBuilder
+				.Entity<Product>()
+				.Navigation(product => product.ShoppingCart)
+				.UsePropertyAccessMode(PropertyAccessMode.Property);
+			
+			modelBuilder
+				.Entity<Product>()
+				.Navigation(product => product.ProductRequests)
+				.UsePropertyAccessMode(PropertyAccessMode.Property);
+			
+			modelBuilder
+				.Entity<Product>()
+				.Navigation(product => product.ProductChangeLogs)
+				.UsePropertyAccessMode(PropertyAccessMode.Property);
+		}
 	}
 }
