8573688dbc314bb838748a09387a1f518ff169b5
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-16 15:34:01 -0600

    Add RelationshipsConfiguration implementation

--- Mux/Relationships/ProductChangelogRelationships.cs
+++ Mux/Relationships/ProductChangelogRelationships.cs
@@ -1,7 +1,25 @@
-﻿namespace Mux.Relationships
+﻿using Microsoft.EntityFrameworkCore;
+using Mux.Model;
+
+namespace Mux.Relationships
 {
-	internal class ProductChangelogRelationships
+	internal class ProductChangelogRelationships : RelationshipsConfiguration
 	{
-		
+		public void Configure(ref ModelBuilder modelBuilder)
+		{
+			modelBuilder
+				.Entity<ProductChangeLog>()
+				.HasOne(recordOfProductMovement => recordOfProductMovement.Product)
+				.WithMany(product => product.ProductChangeLogs)
+				.HasForeignKey(recordOfProductMovement => recordOfProductMovement.ProductId)
+				.OnDelete(DeleteBehavior.Cascade);
+
+			modelBuilder
+				.Entity<ProductChangeLog>()
+				.HasOne(recordOfProductMovement => recordOfProductMovement.Employee)
+				.WithMany(employee => employee.ProductChangeLogs)
+				.HasForeignKey(recordOfProductMovement => recordOfProductMovement.EmployeeId)
+				.OnDelete(DeleteBehavior.SetNull);
+		}
 	}
 }
