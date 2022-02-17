22cf62d4e24f0e3d459c78d12af50c595fd58d18
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-17 11:25:46 -0600

    Add theory InsertNewCategoryWithoutProductResultOk

--- MuxUnitTests/Tables/Categories.cs
+++ MuxUnitTests/Tables/Categories.cs
@@ -1,10 +1,25 @@
-﻿using Mux;
+﻿using System.Linq;
+using Mux;
 using Mux.Model;
+using Xunit;
 
 namespace MuxUnitTests.Tables
 {
     public class Categories
     {
+        [Theory]
+        [InlineData("Leds")]
+        [InlineData("Semiconductores")]
+        [InlineData("Transistores")]
+        public void InsertNewCategoryWithoutProductResultOk(string categoryName)
+        {
+            ICDatabase database = new();
+            int previousCount = database.Categories.Count();
+            InsertNewCategoryIntoTableOnlyWithName(categoryName);
+            int newCount = database.Categories.Count();
+            Assert.True(previousCount < newCount);
+        } 
+        
         void InsertNewCategoryIntoTableOnlyWithName(string name)
         {
             using ICDatabase database = new ICDatabase();
