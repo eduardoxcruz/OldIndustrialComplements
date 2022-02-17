a93e3368c113668e158e21154080848e7b3a379f
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-17 16:26:27 -0600

    Add IEntityTypeConfiguration implementation

--- Mux/EntityTypes/EncapsulationTypeEntityType.cs
+++ Mux/EntityTypes/EncapsulationTypeEntityType.cs
@@ -1,7 +1,28 @@
-﻿namespace Mux.EntityTypes
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Mux.Model;
+
+namespace Mux.EntityTypes
 {
-	public class EncapsulationTypeEntityType
+	public class EncapsulationTypeEntityType : IEntityTypeConfiguration<EncapsulationType>
 	{
-		
+		public void Configure(EntityTypeBuilder<EncapsulationType> builder)
+		{
+			builder.HasKey(encapsulationType => encapsulationType.Id);
+
+			builder.Property(e => e.Id)
+				.ValueGeneratedOnAdd();
+
+			builder.Property(e => e.Name)
+				.HasMaxLength(30)
+				.IsUnicode(false);
+
+			builder.Property(e => e.BodyWidth)
+				.HasMaxLength(30)
+				.IsUnicode();
+
+			builder.Property(e => e.FullDescription)
+				.HasComputedColumnSql("[Name] + ', ' + [BodyWidth]", stored: true);
+		}
 	}
 }
