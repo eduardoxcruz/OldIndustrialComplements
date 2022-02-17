d210990fd164f36abef87aebbff4e9587513f1c8
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-17 16:46:43 -0600

    Add RenamingEncapsulationType migration

---
+++ Mux/Migrations/20220217223521_RenamingEncapsulationType.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Inventory.Migrations
+{
+    public partial class RenamingEncapsulationType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EncapsulationType",
+                table: "Products",
+                newName: "OldEncapsulationType");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OldEncapsulationType",
+                table: "Products",
+                newName: "EncapsulationType");
+        }
+    }
+}
