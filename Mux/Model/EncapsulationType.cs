669d11aa47abd69276e79fde8d5bbcc97497bd5c
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-17 14:31:15 -0600

    Add IOnPropertyChanged implementation

--- Mux/Model/EncapsulationType.cs
+++ Mux/Model/EncapsulationType.cs
@@ -1,7 +1,15 @@
-﻿namespace Mux.Model
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Mux.Model
 {
-	public class EncapsulationType
+	public class EncapsulationType : INotifyPropertyChanged
 	{
-		
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
 	}
 }
