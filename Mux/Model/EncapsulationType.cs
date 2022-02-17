7f0a3e9d7b521df78cf234125f6af87c813b2003
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-17 15:55:53 -0600

    Add properties with getters and setters

--- Mux/Model/EncapsulationType.cs
+++ Mux/Model/EncapsulationType.cs
@@ -5,6 +5,60 @@ namespace Mux.Model
 {
 	public class EncapsulationType : INotifyPropertyChanged
 	{
+		private int _id;
+		private string _name;
+		private string _bodyWidht;
+		private string _fullDescription;
+
+		public int Id
+		{
+			get
+			{
+				return _id;
+			}
+			set
+			{
+				_id = value;
+				OnPropertyChanged(nameof(Id));
+			}
+		}
+		public string Name
+		{
+			get
+			{
+				return _name;
+			}
+			set
+			{
+				_name = value;
+				OnPropertyChanged(nameof(Name));
+			}
+		}
+		public string BodyWidth
+		{
+			get
+			{
+				return _bodyWidht;
+			}
+			set
+			{
+				_bodyWidht = value;
+				OnPropertyChanged(nameof(BodyWidth));
+			}
+		}
+		public string FullDescription
+		{
+			get
+			{
+				return _fullDescription;
+			}
+			set
+			{
+				_fullDescription = value;
+				OnPropertyChanged(nameof(FullDescription));
+			}
+		}
+
 		public event PropertyChangedEventHandler PropertyChanged;
 
 		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
