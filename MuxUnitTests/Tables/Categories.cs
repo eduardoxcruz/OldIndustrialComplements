0b006e7a4ac193a5b43ec1f462223381feda2077
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-17 11:15:41 -0600

    Add InsertNewCategoryIntoTable() method

--- MuxUnitTests/Tables/Categories.cs
+++ MuxUnitTests/Tables/Categories.cs
@@ -1,7 +1,16 @@
-﻿namespace MuxUnitTests.Tables
+﻿using Mux;
+using Mux.Model;
+
+namespace MuxUnitTests.Tables
 {
     public class Categories
     {
-        
+        void InsertNewCategoryIntoTableOnlyWithName(string name)
+        {
+            using ICDatabase database = new ICDatabase();
+            var newCategory = new Category() { Name = name };
+            database.Categories.Add(newCategory);
+            database.SaveChanges();
+        }
     }
 }
