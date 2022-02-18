edc8453f8dcf948a81029bbf4e97e8ea68144e75
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-17 20:53:47 -0600

    Add NavigationPropertiesConfiguration implementation

--- Mux/NavigationProperties/EncapsulationTypeNavigationProperties.cs
+++ Mux/NavigationProperties/EncapsulationTypeNavigationProperties.cs
@@ -1,7 +1,15 @@
-﻿namespace Mux.NavigationProperties
+﻿using Microsoft.EntityFrameworkCore;
+using Mux.Model;
+
+namespace Mux.NavigationProperties
 {
-	public class EncapsulationTypeNavigationProperties
+	public class EncapsulationTypeNavigationProperties : NavigationPropertiesConfiguration
 	{
-		
+		public void Configure(ref ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<EncapsulationType>()
+				.Navigation(encapsulationType => encapsulationType.Products)
+				.UsePropertyAccessMode(PropertyAccessMode.Property);
+		}
 	}
 }
