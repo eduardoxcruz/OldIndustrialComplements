664b8ca11b23237df8fd19588737432c33655874
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-17 18:12:04 -0600

    Add RelationshipsConfiguration implementation

--- Mux/Relationships/ProductRelationships.cs
+++ Mux/Relationships/ProductRelationships.cs
@@ -1,7 +1,17 @@
-﻿namespace Mux.Relationships
+﻿using Microsoft.EntityFrameworkCore;
+using Mux.Model;
+
+namespace Mux.Relationships
 {
-	internal class ProductRelationships
+	internal class ProductRelationships : RelationshipsConfiguration
 	{
-		
+		public void Configure(ref ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Product>()
+				.HasOne(product => product.EncapsulationType)
+				.WithMany(e => e.Products)
+				.HasForeignKey(p => p.EncapsulationTypeId)
+				.OnDelete(DeleteBehavior.ClientCascade);
+		}
 	}
 }
